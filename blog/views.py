4f9a1c39366f4c51363d7158ec31fbc3a49b1bad
yoruneko <hakujyo0518@gmail.com>
2018-02-23 17:48:23 +0800

    update archives and category

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,17 @@
 import markdown
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category
+
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'index.html', context={'post_list': post_list})
 
+
 def home(request):
     return render(request, 'home.html')
 
-def teststring(request):
-    string = u"我在自强学堂学习Django，用它来建网站"
-    return render(request, 'teststring.html', {'string': string})
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -23,3 +22,16 @@ def detail(request, pk):
                                      'markdown.extensions.toc',
                                   ])
     return render(request, 'detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'index.html', context={'post_list': post_list})
