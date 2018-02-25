27e6f825af57fd50c963a49b80142b2c904e8a12
yoruneko <hakujyo0518@gmail.com>
2018-02-25 15:42:32 +0800

    update excerpt and view statistics

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post, Category
 from comments.forms import CommentForm
 
 
+
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'index.html', context={'post_list': post_list})
@@ -16,6 +17,7 @@ def home(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                      'markdown.extensions.extra',
@@ -28,7 +30,7 @@ def detail(request, pk):
     # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
     context = {'post': post,
                'form': form,
-               'comment_list': comment_list
+               'comment_list': comment_list,
                }
     return render(request, 'detail.html', context=context)
 
