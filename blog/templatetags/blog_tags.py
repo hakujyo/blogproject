3c4701cdb20c7ff8d5ac064ef7490ddf11b0e0cc
yaoluying <yaoluying@baidu.com>
2018-03-05 16:10:08 +0800

    update aside

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,22 +1,32 @@
 from django import template
 from ..models import Post, Category
+from users.models import User
 
 register = template.Library()
+
 
 @register.simple_tag
 def get_recent_posts(num=5):
     return Post.objects.all().order_by('-created_time')[:num]
 
+
 @register.simple_tag
 def archives():
     return Post.objects.dates('created_time', 'month', order='DESC')
+
 
 @register.simple_tag
 def get_categories():
     # 别忘了在顶部引入 Category 类
     return Category.objects.all()
 
+
 @register.simple_tag
 def get_post_of_category(cate):
     # 别忘了在顶部引入 Category 类
     return Post.objects.filter(category=cate)
+
+
+@register.simple_tag
+def get_users():
+    return User.objects.all()
