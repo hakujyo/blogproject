e7c9fcf754f3a061ad25247e2b3737ed2deb36ff
yoruneko <hakujyo0518@gmail.com>
2018-04-10 00:15:16 +0800

    update comment

--- comments/views.py
+++ comments/views.py
@@ -11,6 +11,7 @@ def post_comment(request, post_pk):
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
+            comment.name = request.user
             comment.post = post
             comment.save()
             return redirect(post)
