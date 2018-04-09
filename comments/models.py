6a1378a69ccf27d91f95140ee64c8d043bcfdcfa
yoruneko <hakujyo0518@gmail.com>
2018-04-10 00:04:58 +0800

    update

--- comments/models.py
+++ comments/models.py
@@ -5,8 +5,8 @@ from django.conf import settings
 @python_2_unicode_compatible
 class Comment(models.Model):
     name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
-    email = models.EmailField(max_length=255)
-    url = models.URLField(blank=True)
+    # email = models.EmailField(max_length=255)
+    # url = models.URLField(blank=True)
     text = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE,)
