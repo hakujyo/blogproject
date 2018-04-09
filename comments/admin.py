99625f1d6f73283569bc32dcc93a5c91cd499ae3
yoruneko <hakujyo0518@gmail.com>
2018-04-09 23:37:59 +0800

    update

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'text', 'created_time', 'post']
+
+# 把新增的 PostAdmin 也注册进来
+admin.site.register(Comment)
