3468b972d563a63363ff0cb307c83fb3f0e8820f
yoruneko <hakujyo0518@gmail.com>
2018-04-13 15:35:04 +0800

    update edu and gender

---
+++ blog/templatetags/user_tags.py
@@ -0,0 +1,22 @@
+from django import template
+from django.db.models.aggregates import Count
+from django.contrib.contenttypes.models import ContentType
+
+from ..models import Post, Category, Tag
+from users.models import User
+from likes.models import Likes, LikesDetail
+
+register = template.Library()
+from likes.decorator import check_login, check_request
+
+
+CHOICE_GENDER = {1: '男', 2: '女'}
+CHOICE_EDU = {1: '高中或以下学历', 2: '学士', 3: '硕士', 4: '博士', 5: '博士后'}
+
+@register.simple_tag
+def get_gender(author):
+    return CHOICE_GENDER[author.sex]
+
+@register.simple_tag
+def get_edu(author):
+    return CHOICE_EDU[author.education]
