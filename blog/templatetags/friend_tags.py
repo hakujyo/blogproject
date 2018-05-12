5c25d7cf4ae72c11c952c82b3ace95cf5a45b18b
yoruneko <hakujyo0518@gmail.com>
2018-05-12 16:39:43 +0800

    recommand post based on users

--- blog/templatetags/friend_tags.py
+++ blog/templatetags/friend_tags.py
@@ -31,14 +31,26 @@ def is_like(id, user):
     try:
         l = Likes.objects.get(content_type = c, object_id = obj_id)
     except Exception as e:
-        #没有获取到对象，则新增一个Likes对象
         return False
-        # l = Likes(content_type = c, object_id = obj_id)
     try:
         detail = LikesDetail.objects.get(likes = l, user = user)
     except Exception as e:
         return False
-        # detail = LikesDetail(likes = l, user = user, is_like = False)
+    return detail.is_like
+
+@register.simple_tag
+def is_like_post(id, user):
+    obj_type = "post"
+    obj_id = id
+    c = ContentType.objects.get(model=obj_type)
+    try:
+        l = Likes.objects.get(content_type = c, object_id = obj_id)
+    except Exception as e:
+        return False
+    try:
+        detail = LikesDetail.objects.get(likes = l, user = user)
+    except Exception as e:
+        return False
     return detail.is_like
 
 @register.simple_tag
@@ -53,15 +65,6 @@ def is_friend(user, author):
 def get_hot_users():
     pass
 
-
-# class recommandTuple:
-#     def __init__(self, userA, userB, similarity):
-#         self.userA = userA
-#         self.userB = userB
-#         self.similarity = similarity
-#
-#     def __repr__(self):
-#         return repr(( self.userA.username,  self.userB.username, self.similarity))
 
 @register.simple_tag
 def get_similarity_of_user(userA, userB):
@@ -97,6 +100,48 @@ def get_recommand_users(user):
     for user_tuple in user_tuples[:6]:
         recommand_users.append(user_tuple[1])
     return recommand_users
+
+@register.simple_tag
+def get_most_similar_users(user):
+    users = User.objects.all()
+    user_tuples = []
+    for userB in users:
+        if user != userB:
+            temp = get_similarity_of_user(user, userB)
+            user_tuples.append(temp)
+    user_tuples = sorted(user_tuples, key=lambda x: x[2], reverse=True)
+    print(user_tuples)
+    recommand_users = []
+    for user_tuple in user_tuples[:3]:
+        recommand_users.append(user_tuple[1])
+    return user_tuples[:3], recommand_users
+
+@register.simple_tag
+def get_similarity_from_user_tuples(users_tuples, user):
+    for users in users_tuples:
+        if users[1] is user:
+            return users[2]
+    return 0
+
+@register.simple_tag
+def get_recommand_posts_based_on_user(user):
+    users_tuples, similar_users = get_most_similar_users(user)
+    posts = Post.objects.all()
+    post_tuples = []
+    for post in posts:
+        if not is_like_post(post.pk, user):
+            interest_grade = 0
+            for userB in similar_users:
+                if is_like_post(post.pk, userB):
+                    similarity = get_similarity_from_user_tuples(users_tuples, userB)
+                    interest_grade = interest_grade + similarity
+            post_tuples.append((user, post, interest_grade))
+        post_tuples = sorted(post_tuples, key=lambda x: x[2], reverse=True)
+    print(post_tuples)
+    recommand_posts = []
+    for post_tuple in post_tuples[:6]:
+        recommand_posts.append(post_tuple[1])
+    return recommand_posts
 
 @register.simple_tag
 def get_friends(user):
